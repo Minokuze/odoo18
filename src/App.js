4da2a783d14f44b4db39283c6cb4877c5c616c0a
Minokuze <112556616+Minokuze@users.noreply.github.com>
2025-01-22 09:08:49 +0630

    delete no need App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/saga-blue/theme.css'; // PrimeReact Theme
 import 'primereact/resources/primereact.min.css'; // PrimeReact CSS
 import 'primeflex/primeflex.css'
-import SearchBox from './SearchBox.js';
-import backgroundImage from './assets/back.jpg';
-import MenuSide from './MenuSide.js';
 
 function App() {
   return (
